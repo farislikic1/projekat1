f0b34b0f693a6151e64678f65fb38e7298c88b69
Amer Hero <amherhero@gmail.com>
2020-06-27 19:36:55 +0200

    drugi screen, validacija, fale hintovi

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:prviprojekat/Screens/Auth/authentification.dart';
 
 class Home extends StatelessWidget {
+  final String user;
+  final String pass;
+  Home({Key key, @required this.user, @required this.pass}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[100],
+      backgroundColor: hexToColor('#5ba42f'),
       appBar: AppBar(
-        leading: SizedBox(width: 5.0), // important thing
-        backgroundColor: Colors.green[300],
+        leading: new IconButton(
+          icon: Icon(Icons.close),
+          color: Colors.white,
+          onPressed: () => {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Authentification(),
+                ))
+          },
+        ),
+        backgroundColor: hexToColor('#3d422b'),
         elevation: 0.0,
         title: Text('Demo Tech Team 3', style: TextStyle(fontSize: 16.0)),
         actions: <Widget>[
@@ -15,6 +29,34 @@ class Home extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 50.0),
           ),
         ],
+      ),
+      body: Container(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextField(
+              minLines: 10,
+              maxLines: 15,
+              autocorrect: false,
+              decoration: InputDecoration(
+                hintText: 'Hey ' +
+                    user +
+                    ', welcome to second screen. Your password is ' +
+                    pass,
+                filled: true,
+                fillColor: hexToColor('#bdef9a'),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
